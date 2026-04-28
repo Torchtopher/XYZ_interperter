fec86c3a838d3c181a3e53dd7f7bb1c7e1a5842b
surv <mesoscopic@noreply.codeberg.org>
2026-04-28 17:03:16 -0400

    chore: clean up token span handling

--- xyz/tokenizer/__init__.py
+++ xyz/tokenizer/__init__.py
@@ -11,11 +11,16 @@ def peek(file) -> str:
 def tokenize(file) -> list[Token] | None:
     tokens: list[Token] = []
     start: int = 0
+
+    def report(token: Token):
+        tokens.append(token)
+        nonlocal start
+        start = token[1][1]
     while True:
         char: str = file.read(1)
         match char:
             case '':
-                tokens.append((TokenType.EOF, (start, start+1), None))
+                report((TokenType.EOF, (start, start+1), None))
                 return tokens
             case _ if char.isspace():
                 start += 1
@@ -29,30 +34,21 @@ def tokenize(file) -> list[Token] | None:
                         end += 1
                     else:
                         break
-                tokens.append((TokenType.IDENT, (start, end), ident))
-                start = end
+                report((TokenType.IDENT, (start, end), ident))
             case _ if char.isdecimal():
-                num: tuple[int, Token] = tokenize_number(file, start, char)
-                start = num[0]
-                tokens.append(num[1])
+                report(tokenize_number(file, start, char))
             case '.':
                 nextchar: str = peek(file)
                 if nextchar.isdecimal():
-                    num: tuple[int, Token] = tokenize_number(file, start, char)
-                    start = num[0]
-                    tokens.append(num[1])
+                    report(tokenize_number(file, start, char))
                 elif nextchar == '.':
                     file.read(1)
-                    tokens.append(
-                        (TokenType.OP_CONCAT, (start, start+2), None))
-                    start += 2
+                    report(((TokenType.OP_CONCAT, (start, start+2), None)))
                 else:
-                    tokens.append((TokenType.DOT, (start, start+1), None))
+                    report((TokenType.DOT, (start, start+1), None))
                     start += 1
             case "'" | '"':
-                string: tuple[int, Token] = tokenize_string(file, start, char)
-                start = string[0]
-                tokens.append(string[1])
+                report(tokenize_string(file, start, char))
             case '-':
                 if peek(file) == '-':
                     # handle comments
@@ -60,16 +56,14 @@ def tokenize(file) -> list[Token] | None:
                     while file.read(1) != "\n":
                         start += 1
                 else:
-                    tokens.append(
-                        (TokenType.OP_MINUS, (start, start + 1), None))
-                    start += 1
+                    report((TokenType.OP_MINUS, (start, start + 1), None))
             case _:
                 print("Bad token @ char %s! (%s)" % (start, char))
                 return None
     return tokens
 
 
-def tokenize_number(file, start, char) -> tuple[int, Token]:
+def tokenize_number(file, start, char) -> Token:
     frac: bool = char == '.'
     final: str = char
     end: int = start + 1
@@ -84,11 +78,10 @@ def tokenize_number(file, start, char) -> tuple[int, Token]:
             end += 1
         else:
             break
-    return (end,
-            (TokenType.FLOAT if frac else TokenType.INT, (start, end), final))
+    return (TokenType.FLOAT if frac else TokenType.INT, (start, end), final)
 
 
-def tokenize_string(file, start, char) -> tuple[int, Token]:
+def tokenize_string(file, start, char) -> Token:
     final: str = ""
     escape: bool = False
     end: int = start+1
@@ -131,4 +124,4 @@ def tokenize_string(file, start, char) -> tuple[int, Token]:
             escape = True
         else:
             final += nextchar
-    return (end, (TokenType.STRING, (start, end), final))
+    return (TokenType.STRING, (start, end), final)
