16a868d0e174c0d832afbd894510f7ed19c2093c
ncssm-mor-NyE9DoBlxs3M <245083691+ncssm-mor-NyE9DoBlxs3M@users.noreply.github.com>
2026-05-09 18:42:55 -0400

    tweak: AST change to account for variable creation

--- xyz/parser/ast.py
+++ xyz/parser/ast.py
@@ -76,7 +76,7 @@ class GroupedExpr(NamedTuple):
     # no value because can't know what it will be
 
 type File = Block
-type Statement = (SetStatement | FunctionCall | Break | Block | WhileLoop | RepeatLoop | IfStatement | ForLoop | FunctionDef )
+type Statement = (Definition | SetStatement | FunctionCall | Break | Block | WhileLoop | RepeatLoop | IfStatement | ForLoop | FunctionDef )
 type ReturnStatement = Expression
 
 class Block(NamedTuple):
@@ -87,6 +87,13 @@ class Lambda(NamedTuple):
     parameters: list[str]
     extra: str | None
     block: Block
+
+# function definitions are const Definitions if their name is just a variable name
+# otherwise they are SetStatements
+class Definition(NamedTuple):
+    const: bool
+    var: list[Var]
+    value: list[Expression]
 
 class SetStatement(NamedTuple):
     var: list[Access]
